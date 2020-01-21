32a60b8cf2aad988c64b57c70c8f06da06aba630
HakanSahin11 <hakan.sahin994@gmail.com>
2020-01-21 09:04:36 +0100

    Initiate

---
+++ API Setup User config/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using API_Setup_User_config.Models;
+using MongoDB.Driver.Linq;
+
+namespace API_Setup_User_config.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public List<List<UserClass>> Database = new List<List<UserClass>>();
+        public IMongoDatabase client = new MongoClient("mongodb://GateKeeper:silvereye@localhost:27017").GetDatabase("Virksomhed");
+
+        void dbSetup()
+        {
+            var userQuery = from c in client.GetCollection<UserClass>(UserClass.Name).AsQueryable()
+                            select c;
+            foreach (UserClass output in userQuery)
+            {
+                List<UserClass> userClasses = new List<UserClass>{ output };
+                Database.Add(userClasses);
+            }
+        }
+        // GET: api/User
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                dbSetup();
+                return Ok(Database);
+            }
+            catch (Exception e)
+            {
+                return Ok($"Error! \n\n{e.Message}");
+            }
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult Get(int id)
+        {
+
+            return Ok (from c in client.GetCollection<UserClass>(UserClass.Name).AsQueryable()
+                   where c._id == id
+                   select c);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API Setup User config/Models/UserClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Setup_User_config.Models
+{
+    public class UserClass
+    {
+        public static readonly string Name = "Users";
+        public UserClass(int id, string email, string password, string userType, string firstName, string lastName, char gender, string country, string city, string address, string jobTitle, int age)
+        {
+            _id = id;
+            Email = email;
+            Password = password;
+            UserType = userType;
+            FirstName = firstName;
+            LastName = lastName;
+            Gender = gender;
+            Country = country;
+            City = city;
+            Address = address;
+            JobTitle = jobTitle;
+            Age = age;
+        }
+
+        public int _id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string JobTitle { get; set; }
+        public int Age { get; set; }
+    }
+}
